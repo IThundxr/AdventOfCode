bd4a12e8782224bb7cfe36565bd46f248d83a170
IThundxr <contact@ithundxr.dev>
2023-12-02 20:56:26 -0500

    it works :)

--- src/day2.rs
+++ src/day2.rs
@@ -1,7 +1,20 @@
+use std::time::SystemTime;
+
 fn main() {
-    let input = include_str!("../input/day2.txt");
+    let time_start = SystemTime::now();
+
+    let use_example = false;
+
+    let input: &str;
+
+    if !use_example {
+        input = include_str!("../input/day2.txt");
+    } else {
+        input = include_str!("../input/day2_example.txt");
+    }
 
     let mut game_sum: i32 = 0;
+    let mut game_power_thing: i32 = 0;
 
     // Game 1: 1 green, 2 red, 6 blue; 4 red, 1 green, 3 blue; 7 blue, 5 green; 6 blue, 2 red, 1 green
     for line in input.lines() {
@@ -18,6 +31,9 @@ fn main() {
         let rolls = other_input.split("; ");
 
         let mut game_possible = true;
+
+        let mut lowest_amount = Cubes { red: 0, green: 0, blue: 0 };
+        let mut power_of_cubes = 0;
 
         // 1 green, 2 red, 6 blue
         for roll in rolls {
@@ -45,17 +61,27 @@ fn main() {
                 }
             }
 
+            lowest_amount.red = lowest_amount.red.max(cubes.red);
+            lowest_amount.green = lowest_amount.green.max(cubes.green);
+            lowest_amount.blue = lowest_amount.blue.max(cubes.blue);
+
             if !cubes.is_possible() {
                 game_possible = false;
             }
         }
 
+        power_of_cubes = lowest_amount.red * lowest_amount.green * lowest_amount.blue;
+
+        game_power_thing += power_of_cubes;
+
         if game_possible {
-            game_sum += game_number
+            game_sum += game_number;
         }
     }
 
-    println!("{}", game_sum)
+    let time_taken = SystemTime::now().duration_since(time_start).unwrap().as_micros();
+
+    println!("Part 1: {} \nPart 2: {} \n\nTook a total of {}ms to complete", game_sum, game_power_thing, time_taken);
 }
 
 impl Cubes {
