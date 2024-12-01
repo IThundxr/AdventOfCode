440f9b25f8e1ff048c0f6ee539cac7c108167ca4
IThundxr <me@ithundxr.dev>
2024-12-01 11:48:40 -0500

    Advent Of Code Day 1

---
+++ src/bin/01.rs
@@ -0,0 +1,78 @@
+advent_of_code::solution!(1);
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let mut result = 0;
+
+    let mut first_list_sorted: Vec<u32> = Vec::new();
+    let mut second_list_sorted: Vec<u32> = Vec::new();
+
+    for line in input.lines() {
+        let (first_list, second_list) = line.split_once("   ")?;
+        first_list_sorted.push(first_list.parse::<u32>().unwrap());
+        second_list_sorted.push(second_list.parse::<u32>().unwrap());
+    }
+
+    first_list_sorted.sort();
+    second_list_sorted.sort();
+
+    for i in 0..first_list_sorted.len() {
+        let first_number = first_list_sorted.get(i).unwrap();
+        let second_number = second_list_sorted.get(i).unwrap();
+
+        let biggest_number = first_number.max(second_number);
+        let smallest_number = first_number.min(second_number);
+
+        result += biggest_number - smallest_number;
+    }
+
+    Some(result)
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let mut result = 0;
+
+    let mut first_list: Vec<u32> = Vec::new();
+    let mut second_list: Vec<u32> = Vec::new();
+
+    for line in input.lines() {
+        let (first, second) = line.split_once("   ")?;
+
+        let first_num = first.parse::<u32>().unwrap();
+        let second_num = second.parse::<u32>().unwrap();
+
+        first_list.push(first_num);
+        second_list.push(second_num);
+    }
+
+
+    for first in first_list {
+        let mut second_count: u32 = 0;
+
+        for &second in &second_list {
+            if first == second {
+                second_count += 1;
+            }
+        }
+
+        result += first * second_count;
+    }
+
+    Some(result)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(11));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(31));
+    }
+}
