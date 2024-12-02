daf720639d18194f1eb4657fe2ce1e1f71a64ff5
IThundxr <me@ithundxr.dev>
2024-12-02 11:59:25 -0500

    WIP pt 2

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,24 +1,55 @@
 advent_of_code::solution!(2);
 
+pub fn is_safe(nums: &Vec<isize>) -> bool {
+    let first_difference = nums[1] - nums[0];
+
+    for i in 1..nums.len() {
+        let difference = nums[i] - nums[i - 1];
+
+        if difference.abs() < 1
+            || difference.abs() > 3
+            || (difference.signum() != first_difference.signum())
+        {
+            return false;
+        }
+    }
+
+    true
+}
+
 pub fn part_one(input: &str) -> Option<usize> {
     let result = input
         .lines()
-        .filter(|line| {
+        .filter(|&line| {
             let nums = line
                 .split_ascii_whitespace()
                 .map(|s| s.parse::<isize>().unwrap())
                 .collect::<Vec<_>>();
 
-            let first_difference = nums[1] - nums[0];
+            is_safe(&nums)
+        })
+        .count();
 
-            for i in 1..nums.len() {
-                let difference = nums[i] - nums[i - 1];
+    Some(result)
+}
 
-                if difference.abs() < 1
-                    || difference.abs() > 3
-                    || (difference.signum() != first_difference.signum())
-                {
-                    return false;
+pub fn part_two(input: &str) -> Option<usize> {
+    let result = input
+        .lines()
+        .filter(|&line| {
+            let nums = line
+                .split_ascii_whitespace()
+                .map(|s| s.parse::<isize>().unwrap())
+                .collect::<Vec<_>>();
+
+            if !is_safe(&nums) {
+                for i in 0..nums.len() {
+                    let mut new_nums = nums.clone();
+                    new_nums.remove(i);
+
+                    if is_safe(&new_nums) {
+                        return false;
+                    }
                 }
             }
 
@@ -27,10 +58,6 @@ pub fn part_one(input: &str) -> Option<usize> {
         .count();
 
     Some(result)
-}
-
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
 }
 
 #[cfg(test)]
@@ -46,6 +73,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(4));
     }
 }
